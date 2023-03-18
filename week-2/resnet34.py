edaca0efff505cec3bfcff7b1ab394cb223a52da
Abhik <abhiksark@gmail.com>
2023-03-18 11:59:52 +0530

    updated Trainer

--- week-2/resnet34.py
+++ week-2/resnet34.py
@@ -1,32 +1,6 @@
 import torch.nn as nn
 
-class ResNetBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, stride=1, downsample=None):
-        super(ResNetBlock, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(out_channels)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(out_channels)
-        self.downsample = downsample
-
-    def forward(self, x):
-        residual = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-
-        if self.downsample is not None:
-            residual = self.downsample(x)
-
-        out += residual
-        out = self.relu(out)
-
-        return out
+from resnetblock import ResNetBlock
 
 class Resnet34(nn.Module):
     def __init__(self, num_classes=1000):
@@ -35,15 +9,13 @@ class Resnet34(nn.Module):
             nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False),
             nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=1, padding=1)
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         )
-        # input: 224x224x3 output: 56x56x64
         self.conv2 = nn.Sequential(
             ResNetBlock(64, 64),
             ResNetBlock(64, 64),
             ResNetBlock(64, 64)
         )
-        # input: 56x56x64 output: 28x28x128
         self.conv3 = nn.Sequential(
             ResNetBlock(64, 128, stride=2, downsample=nn.Sequential(
                 nn.Conv2d(64, 128, kernel_size=1, stride=2, bias=False),
@@ -51,15 +23,17 @@ class Resnet34(nn.Module):
             )),
             ResNetBlock(128, 128),
             ResNetBlock(128, 128),
-            ResNetBlock(128, 128),
             ResNetBlock(128, 128)
         )
-        # input: 28x28x128 output: 14x14x256
         self.conv4 = nn.Sequential(
             ResNetBlock(128, 256, stride=2, downsample=nn.Sequential(
                 nn.Conv2d(128, 256, kernel_size=1, stride=2, bias=False),
                 nn.BatchNorm2d(256)
             )),
+            ResNetBlock(256, 256),
+            ResNetBlock(256, 256),
+            ResNetBlock(256, 256),
+            ResNetBlock(256, 256),
             ResNetBlock(256, 256)
         )
         self.conv5 = nn.Sequential(
@@ -67,9 +41,10 @@ class Resnet34(nn.Module):
                 nn.Conv2d(256, 512, kernel_size=1, stride=2, bias=False),
                 nn.BatchNorm2d(512)
             )),
+            ResNetBlock(512, 512),
             ResNetBlock(512, 512)
         )
-        self.avg_pool = nn.AvgPool2d(7, stride=1)
+        self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
         self.fc = nn.Linear(512, num_classes)
 
     def forward(self, x):
